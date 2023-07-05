ea115532d77394b9cb1b0c69806b01c4af2183ba
Ryan Mitchell <ryanm4398@gmail.com>
2023-07-05 15:10:18 -0400

    completed functions for local storage

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,24 @@
 var searchButton = document.getElementById('searchButton');
 var API_KEY = '3ba56d777134dbd577f935196cde9412';
 
+function saveSearch() {
+  var citySearch = document.querySelector('.citySearch');
+  var citySearchInput = citySearch.value;
+  localStorage.setItem('searchedCity', citySearchInput);
+  renderLocal();
+}
+
+function renderLocal() {
+  //these are to grab the necessary items to create the search history buttons
+  var storageCard = document.querySelector('.storageCard');
+  var recentSearch = localStorage.getItem('searchedCity');
+  
+  //creating a button and using the local storage getItem for the text of the button, then appending to the card
+  var historyButton = document.createElement('button');
+  historyButton.textContent = recentSearch;
+  storageCard.appendChild(historyButton);
+}
+
 function currentWeatherCompleter(lat, lon) {
   var requestUrlTwo = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
 
@@ -15,7 +33,7 @@ function currentWeatherCompleter(lat, lon) {
       currentWeatherCard.style.border = '2px solid white';
 
       var currentTitle = document.createElement('h2');
-      currentTitle.textContent = data.name;
+      currentTitle.textContent = data.name + " (Today)";
 
       var currentImage = document.createElement('img');
       currentImage.src = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png';
@@ -38,9 +56,10 @@ function currentWeatherCompleter(lat, lon) {
   }
 
 function currentWeatherGetApi() {
-  var citySearch = document.getElementById('citySearch');
+  var citySearch = document.querySelector('.citySearch');
   var citySearchInput = citySearch.value;
-    var requestUrlOne = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearchInput}&appid=${API_KEY}`;
+
+  var requestUrlOne = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearchInput}&appid=${API_KEY}`;
   
     fetch(requestUrlOne)
       .then(function (response) {
@@ -55,4 +74,7 @@ function currentWeatherGetApi() {
       );
   }
 
-searchButton.addEventListener('click', currentWeatherGetApi);
+searchButton.addEventListener('click', function() {
+  currentWeatherGetApi(); 
+  saveSearch(); 
+});
